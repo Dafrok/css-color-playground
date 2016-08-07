e8d7e6eed21a6c0429260d012b308169328650d0
Dafrok <o.o@mug.dog>
2016-08-07 13:18:02 +0800

    code

--- src/js/index.js
+++ src/js/index.js
@@ -1,80 +1,45 @@
 import Vue from 'vue'
 import Color from 'color-factory'
 
-const app = new Vue({
-  el: '#app',
-  data () {
-    return {
-      color: {
-        HEX: '',
-        RGB: '',
-        RGBA: '',
-        HSL: '',
-        HSLA: '',
-        STRING: ''
+function createApp () {
+  const colorTypes = ['HEX', 'RGB', 'RGBA', 'HSL', 'HSLA', 'STRING']
+  function createData() {
+    const data = {color: {}}
+    colorTypes.forEach(key => data.color[key] = '')
+    return data
+  }
+
+  function createObserver() {
+    const observer = {}
+    colorTypes.forEach(key => observer[key] = {
+      get () {
+        return this.color[key]
+      },
+      set (val) {
+        this.setColor(val)
       }
-    }
-  },
-  methods: {
-    setColor (val) {
+    })
+    return observer
+  }
+
+  function createSetter() {
+    return function (val) {
       const color = new Color(val)
-      console.log(val)
-      this.color.HEX = color.toHEX()
-      this.color.RGB = color.toRGB()
-      this.color.RGBA = color.toRGBA()
-      this.color.HSL = color.toHSL()
-      this.color.HSLA = color.toHSLA()
-      this.color.STRING = color.toSTRING()
-    }
-  },
-  computed: {
-    HEX: {
-      get () {
-        return this.color.HEX
-      },
-      set (val) {
-        this.setColor(val)
-      }
-    },
-    RGB: {
-      get () {
-        return this.color.RGB
-      },
-      set (val) {
-        this.setColor(val)
-      }
-    },
-    RGBA: {
-      get () {
-        return this.color.RGBA
-      },
-      set (val) {
-        this.setColor(val)
-      }
-    },
-    HSL: {
-      get () {
-        return this.color.HSL
-      },
-      set (val) {
-        this.setColor(val)
-      }
-    },
-    HSLA: {
-      get () {
-        return this.color.HSLA
-      },
-      set (val) {
-        this.setColor(val)
-      }
-    },
-    STRING: {
-      get () {
-        return this.color.STRING
-      },
-      set (val) {
-        this.setColor(val)
-      }
+      colorTypes.forEach(key => this.color[key] = color[`to${key}`]())
     }
   }
-})
+  return new Vue({
+    el: '#app',
+    data () {
+      return createData.call(this)
+    },
+    get computed() {
+      return createObserver.call(this)
+    },
+    get methods() {
+      return { setColor: createSetter.call(this) }
+    }
+  })
+}
+
+const app = createApp()

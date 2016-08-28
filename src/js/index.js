429ce89306b0b403803b114543f79ca3d5b89636
Dafrok <o.o@mug.dog>
2016-08-28 12:56:35 +0800

    Minify source code

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,17 @@
 import Vue from 'vue'
 import Color from 'color-factory'
 
-const colorTypes = ['HEX', 'RGB', 'RGBA', 'HSL', 'HSLA', 'STRING']
 export default new Vue({
   el: '#app',
   data () {
-    const data = {color: {}}
-    colorTypes.forEach(key => data.color[key] = '')
-    return data
+    return { inputValue: '' }
   },
   get computed() {
     const observer = {}
-    colorTypes.forEach(key => observer[key] = {
-      get () {
-        return this.color[key]
-      },
-      set (val) {
-        this.setColor(val)
-      }
+    const colorTypes = ['HEX', 'RGB', 'RGBA', 'HSL', 'HSLA', 'STRING']
+    colorTypes.forEach(key => observer[key] = function () {
+      return (new Color(this.inputValue))[`to${key}`]()
     })
     return observer
-  },
-  methods: {
-    setColor(val) {
-      const color = new Color(val)
-      colorTypes.forEach(key => this.color[key] = color[`to${key}`]())
-    }
   }
 })
